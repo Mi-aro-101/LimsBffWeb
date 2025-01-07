ae6aa30a2138084012d6b68c62d30c98e6e3baab
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2025-01-07 11:23:45 +0300

    Crud Departement

---
+++ Controllers/DepartementBffController.cs
@@ -0,0 +1,58 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DepartementBff.Models;
+using System.Linq;
+using Departement.Utils;
+using System.Text.Json;
+
+namespace LimsBffWeb.Controllers;
+[ApiController]
+[Route("/api/departement")]
+public class DepartementBffController : Controller
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _departementServiceUrl = "http://localhost:5189/api/Departement"; // Replace with your ProductService URL
+
+
+    public DepartementBffController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    [HttpGet]
+    public async Task<ActionResult> GetDepartement()
+    {
+        ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(_departementServiceUrl);
+        apiResponse.HandleResponse<List<DepartementDto>>();
+        if (apiResponse == null) return NotFound();
+        
+        return Ok(apiResponse);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetDepartementDetails(int id)
+    {
+        string requestUri = $"{_departementServiceUrl}/{id}";
+        ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(requestUri);
+        apiResponse.HandleResponse<DepartementDto>();
+        if (apiResponse == null) return NotFound();
+        
+        return Ok(apiResponse);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateDepartement(DepartementDto departementDto)
+    {
+        var response = await _httpClient.PostAsJsonAsync(_departementServiceUrl, departementDto);
+        using var responseStream = await response.Content.ReadAsStreamAsync();
+        ApiResponse? apiResponse = await JsonSerializer.DeserializeAsync<ApiResponse>(responseStream);
+        if (apiResponse?.Data != null)
+        {
+            apiResponse.HandleResponse<DepartementDto>();
+            DepartementDto departement = (DepartementDto)apiResponse.Data;
+            return Ok(apiResponse);
+        }
+        else return BadRequest("Ohatran'ny nisy olana tao a");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase; // Default
+    // To use PascalCase
+    options.JsonSerializerOptions.PropertyNamingPolicy = null;
+});
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddHttpClient(); // Register HttpClient here
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
