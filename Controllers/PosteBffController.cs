72f8fe0ace37350d06a8420a618023ec1065c107
Liantsiky <malalatsiky11@gmail.com>
2025-01-08 07:15:35 +0300

    devposte 0.1

---
+++ Controllers/PosteBffController.cs
@@ -0,0 +1,84 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LimsBffWeb.Models;
+using System.Linq;
+using LimsBffWeb.Utils;
+using System.Text.Json;
+
+namespace LimsBffWeb.Controllers;
+[ApiController]
+[Route("/api/poste")]
+public class PosteBffController : Controller
+{
+    private readonly HttpClient _httpClient;
+    private readonly string _posteServiceUrl = "http://localhost:5201/api/Poste"; // Replace with your ProductService URL
+
+
+    public PosteBffController(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    [HttpGet]
+    public async Task<ActionResult> GetPoste(int position, int pageSize)
+    {
+        ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(_posteServiceUrl+$"?position={position}&pageSize={pageSize}");
+        apiResponse.HandleResponse<List<PosteDto>>();
+        if (apiResponse == null) return NotFound();
+        
+        return Ok(apiResponse);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult> GetPosteDetails(int id)
+    {
+        string requestUri = $"{_posteServiceUrl}/{id}";
+        ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(requestUri);
+        apiResponse.HandleResponse<PosteDto>();
+        if (apiResponse == null) return NotFound();
+        
+        return Ok(apiResponse);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreatePoste(PosteDto posteDto)
+    {
+        var response = await _httpClient.PostAsJsonAsync(_posteServiceUrl, posteDto);
+        using var responseStream = await response.Content.ReadAsStreamAsync();
+        ApiResponse? apiResponse = await JsonSerializer.DeserializeAsync<ApiResponse>(responseStream);
+        if (apiResponse?.Data != null)
+        {
+            apiResponse.HandleResponse<PosteDto>();
+            PosteDto poste = (PosteDto)apiResponse.Data;
+            return Ok(apiResponse);
+        }
+        else return BadRequest("Ohatran'ny nisy olana tao a");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeletePoste(int id,int pageSize)
+    {
+        string requestUri = $"{_posteServiceUrl}/{id}";
+        HttpResponseMessage response = await _httpClient.DeleteAsync(requestUri);
+        response.EnsureSuccessStatusCode();
+        
+        return await GetPoste(0,pageSize);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> EditPoste(int? id, PosteDto posteDto)
+    {
+        string requestUri = $"{_posteServiceUrl}/{id}";
+        var response = await _httpClient.PutAsJsonAsync(requestUri, posteDto);
+        using var responseStream = await response.Content.ReadAsStreamAsync();
+        ApiResponse? apiResponse = await JsonSerializer.DeserializeAsync<ApiResponse>(responseStream);
+        if (apiResponse?.Data != null)
+        {
+            apiResponse.HandleResponse<PosteDto>();
+            PosteDto poste = (PosteDto)apiResponse.Data;
+            return Ok(apiResponse);
+        }
+        else return BadRequest("Ohatran'ny nisy olana tao a");
+    }
+}
