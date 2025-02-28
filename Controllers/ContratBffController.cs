16a7346329a0b5d90be78418f320b7fdbed6bfbb
tonnyMickael <102615720+tonnyMickael@users.noreply.github.com>
2025-02-28 10:21:18 +0300

    envoie

---
+++ Controllers/ContratBffController.cs
@@ -0,0 +1,46 @@
+﻿using LimsBffWeb.Models;
+using LimsUtils.Api;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace LimsBffWeb.Controllers
+{
+    [ApiController]
+    [Route("api/contrat")]
+    public class ContratBffController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _contratURL = "http://localhost:5290/api/contrat";
+
+        public ContratBffController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpPost("ajout")]
+        public async Task<ActionResult> AddContrat(ContratDto contrat)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{_contratURL}/new", contrat);
+            using var responseStream = await response.Content.ReadAsStreamAsync();
+            ApiResponse? apiResponse = await JsonSerializer.DeserializeAsync<ApiResponse>(responseStream);
+            if (apiResponse?.Data != null)
+            {
+                apiResponse.HandleResponse<ContratDto>();
+                ContratDto departement = (ContratDto)apiResponse.Data;
+                return Ok(apiResponse);
+            }
+            else return BadRequest("Ohatran'ny nisy olana tao a");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> GetAllContrat(int position, int pagesize)
+        {
+            ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(_contratURL + "?position=" + position + "&pageSize=" + pagesize);
+            apiResponse.HandleResponse<List<ContratDto>>();
+            if (apiResponse == null) return NotFound();
+            return Ok(apiResponse);
+        }
+
+    }
+}
---
+++ Models/RecuDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LimsBffWeb.Models
+{
+    public class RecuDto
+    {
+        //reception espece
+        [JsonPropertyName("montantRecu")]
+        public double montantRecu { get; set; }
+
+        //reception mobile 
+        [JsonPropertyName("referenceMobile")]
+        public int referenceMobile { get; set; }
+
+        //reception virement
+        [JsonPropertyName("referenceOV")]
+        public string? referenceOV { get; set; }
+
+        //attribut commun
+        [JsonPropertyName("referenceEtatDecompte")]
+        public string? referenceEtatDecompte { get; set; }
+        [JsonPropertyName("id_paiement")]
+        public int id_paiement { get; set; }
+        [JsonPropertyName("email")]
+        public string? email { get; set; }
+    }
+}
