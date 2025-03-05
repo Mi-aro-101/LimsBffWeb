c47ce5061446d23acf289edb82d386d751d62ec3
Mi-aro-101 <miaroramanandraitsiory@gmail.com>
2025-03-05 09:12:35 +0300

    Prestation details

--- Controllers/PrestationService/PrestationBffController.cs
+++ Controllers/PrestationService/PrestationBffController.cs
@@ -25,4 +25,16 @@ public class PrestationController : Controller
         }
         return Ok(apiResponse);
     }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<ApiResponse>> GetPrestation(int id)
+    {
+        string requestUri = $"{_prestationUrlService}/{id}";
+        ApiResponse? apiResponse = await _httpClient.GetFromJsonAsync<ApiResponse>(requestUri);
+        if (apiResponse?.IsSuccess == false || apiResponse == null)
+        {
+            return BadRequest("Une erreur s'est produite lors de la récupération de donnée : Prestation");
+        }
+        return Ok(apiResponse);
+    }
 }
